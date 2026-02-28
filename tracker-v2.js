01dd4662dfba2915a66e1a6c4f9da2d842644f40
Piyush <ptembhare746@gmail.com>
2026-02-28 22:35:22 +0530

    Update tracker-v2.js

--- tracker-v2.js
+++ tracker-v2.js
@@ -198,8 +198,9 @@ function calculatePoints() {
     var percentage = habits.length > 0 ? (completed / habits.length) * 100 : 0;
     var dayPoints = 0;
     
-    if (percentage === 100) dayPoints = 20;
-    else if (percentage >= 80) dayPoints = 10;
+    if (percentage === 100) dayPoints = 30;
+    else if (percentage >= 80) dayPoints = 15;
+    else if (percentage >= 60) dayPoints = 10;
     else if (percentage >= 50) dayPoints = 5;
     
     monthlyPoints += dayPoints;
@@ -717,4 +718,5 @@ function createProgressCharts() {
     });
   }
 }
+
 
